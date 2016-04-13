278efb8106aba1512c5386e5951b9fd62204995a
David Barnett <david@barnett.net.nz>
2016-04-14 09:27:23 +1200

    start of implementation of query-file

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,30 @@
+//! # liquery-file
+//!
+
 extern crate liquery;
 extern crate mime_guess;
 
+use std::path::Path;
+use std::fs::{File, Metadata};
 
+use liquery::Queryable;
+
+struct QueryFile {
+    metadata: Metadata,
+}
+
+impl QueryFile {
+    pub fn new(path: &Path) -> std::io::Result<Self> {
+        let file = try!(File::open(path));
+        Ok(QueryFile { metadata: try!(file.metadata()) })
+    }
+}
+
+impl Queryable for QueryFile {
+    fn query(&self, key: &str) -> Option<String> {
+        match key {
+            "size" => Some(format!("{}", self.metadata.len())),
+            _ => None,
+        }
+    }
+}
